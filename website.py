d0e3a4253d08386841ac5d078a303bf7c3a590a2
Pseudophoenix <waynerooney0089@gmail.com>
2025-08-13 22:55:43 +0530

    Thumbnails added

--- website.py
+++ website.py
@@ -1,10 +1,18 @@
 import streamlit as st
 import pickle
+import requests
 movies_list=pickle.load(open("movies.pkl","rb"))
 # print(movies_list)
 import pandas as pd
 movies=pd.DataFrame(movies_list)
-
+def fetch_poster(movie_title):
+    resp=requests.get(
+        f"https://www.omdbapi.com/?t={movie_title}&apikey=7014fa2c&plot=full").json()
+    # print(resp)
+    if "Poster" in resp:
+         return resp['Poster']
+    else:
+        return ""
 st.title("Movie Recommender System")
 option=st.selectbox("Enter the movie name:",set(movies_list['title']))
 # st.text("")
@@ -14,11 +22,24 @@ def recommend(movie):
     
     ans=sorted(list(enumerate(similarity[index])),reverse=True,key=lambda x:x[1])[1:6]
     l=[]
+    re_pos=[]
     for i in ans:
         l.append(movies.iloc[i[0]]['title'])
-    return l
+        re_pos.append(fetch_poster(movies.iloc[i[0]]['title']))
+    return l,re_pos
 
 if st.button("Recommend 5 movies"):
-    recommendations=recommend(option)
-    for i in recommendations:
-        st.write(i)
+    names,pos=recommend(option)
+    col1, col2, col3=st.columns(3)
+    with col1:
+        st.header(names[0])
+        if pos[0]:
+            st.image(pos[0])
+    with col2:
+        st.header(names[1])
+        if pos[1]:
+            st.image(pos[1])
+    with col3:
+        st.header(names[2])
+        if pos[2]:
+            st.image(pos[2])
